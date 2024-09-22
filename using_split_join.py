406124d3ac9f48a6c257ba5ef7937c17d08dd26f
Alisha210302 <alishabandal95@gmail.com>
2024-09-22 12:35:13 +0530

    Add files via upload

---
+++ using_split_join.py
@@ -0,0 +1,11 @@
+text = "asdfjdg dsfhsuyf fhsgvf".split()
+print(text)
+text1 = "asdfjdg dsfhsuyf fhsgvf".split(' ')
+print(text1)
+text2 = "asdfjdg,dsfhsuyf,fhsgvf".split(',')
+print(text2)
+
+lst = ['asdfjdg', 'dsfhsuyf', 'fhsgvf']
+a = "".join(lst)
+print(a)
+
